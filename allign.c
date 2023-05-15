f4682b1e3bad331ee7bd87f4adac631415343301
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-05-15 10:37:31 +0530

    Add files via upload

---
+++ allign.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+
+    int mm1 = 11011;
+    int mm2 = 111;
+    int mm3 = 11;
+
+    printf("%10d %d\n", mm1, mm1);
+    printf("%10d %d\n", mm2, mm2);
+    printf("%10d %d\n", mm3, mm3);
+
+    exit(EXIT_SUCCESS);
+}
